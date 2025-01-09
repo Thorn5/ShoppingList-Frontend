d4aa541f6fa18ad0e6f1d3a7786c8e07bda2c1e5
Trygve Horn <trygvehorn@gmail.com>
2025-01-09 12:01:53 +0100

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.jsx
+import './App.css'
+import ShoppingList from './components/ShoppingList'
+
+function App() {
+
+  return (
+    <>
+      <ShoppingList/>
+    </>
+  )
+}
+
+export default App
